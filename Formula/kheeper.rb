cb215c203b3a6a448a19f1fb0aa936655766258f
Mandi B. Toro <mandi@kheeper.com>
2026-05-17 19:04:57 -0500

    Update kheeper to v0.8.0

--- Formula/kheeper.rb
+++ Formula/kheeper.rb
@@ -1,15 +1,15 @@
 class Kheeper < Formula
   desc "Smart OCI registry client for bootable container images"
   homepage "https://kheeper.com"
-  version "0.7.0"
+  version "0.8.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kheepercom/homebrew-tap/releases/download/v0.7.0/kheeper_v0.7.0_darwin_arm64.tar.gz"
-      sha256 "592f85b8e9fd4f6f0d02a07c5e17b6d8abd3c2b199279e53a1af38319c86e08c"
+      url "https://github.com/kheepercom/homebrew-tap/releases/download/v0.8.0/kheeper_v0.8.0_darwin_arm64.tar.gz"
+      sha256 "e94d9d4d17725b9043660a9e9bc6cc7562a4b8a5c77a5fcce7f64972f73ff027"
     else
-      url "https://github.com/kheepercom/homebrew-tap/releases/download/v0.7.0/kheeper_v0.7.0_darwin_amd64.tar.gz"
-      sha256 "db63e4eca9eeef408ed0ba675df6b3919fcedb9954717ced9424f26435ebd51b"
+      url "https://github.com/kheepercom/homebrew-tap/releases/download/v0.8.0/kheeper_v0.8.0_darwin_amd64.tar.gz"
+      sha256 "3199a77e6999a0074a0065ff135add49ca44876d9ece1272c79ff60aa14c91fa"
     end
   end
 
